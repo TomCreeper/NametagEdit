755a67b6b996c555c099e21067f84c93440897aa
Tominous <18189490+TomCreeper@users.noreply.github.com>
2019-03-17 09:42:45 +0000

    Update Metrics.java

--- src/main/java/com/nametagedit/plugin/metrics/Metrics.java
+++ src/main/java/com/nametagedit/plugin/metrics/Metrics.java
@@ -103,11 +103,11 @@ public class Metrics {
             // Every server gets it's unique random id.
             config.addDefault("serverUuid", UUID.randomUUID().toString());
             // Should failed request be logged?
-            config.addDefault("logFailedRequests", false);
+            config.addDefault("logFailedRequests", true);
             // Should the sent data be logged?
-            config.addDefault("logSentData", false);
+            config.addDefault("logSentData", true);
             // Should the response text be logged?
-            config.addDefault("logResponseStatusText", false);
+            config.addDefault("logResponseStatusText", true);
 
             // Inform the server owners about bStats
             config.options().header(
@@ -124,9 +124,9 @@ public class Metrics {
         // Load the data
         enabled = config.getBoolean("enabled", true);
         serverUUID = config.getString("serverUuid");
-        logFailedRequests = config.getBoolean("logFailedRequests", false);
-        logSentData = config.getBoolean("logSentData", false);
-        logResponseStatusText = config.getBoolean("logResponseStatusText", false);
+        logFailedRequests = config.getBoolean("logFailedRequests", true);
+        logSentData = config.getBoolean("logSentData", true);
+        logResponseStatusText = config.getBoolean("logResponseStatusText", true);
 
         if (enabled) {
             boolean found = false;
